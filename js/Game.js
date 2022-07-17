11c335c114773b7e1db341af4f20ac9b2dab54cc
Jhonatan Castillo <jhonatanbattako@gmail.com>
2022-07-17 18:12:21 -0500

    Se reorganizo la presentacion del juego

--- js/Game.js
+++ js/Game.js
@@ -1,20 +1,18 @@
-import {Player} from './Player.js';
 import { Round } from "/js/Round.js";
 
 
 export class Game{
     constructor(){
-        this.player = null;
         this.round = new Round(0);
         this.isOn = false;
     }
 
-    startGame(namePlayer){
-        this.player = new Player(namePlayer,0);
-        localStorage.setItem("player", JSON.stringify(this.player));
+    startGame(){
+        //this.player = new Player(namePlayer,0);
+        //localStorage.setItem("player", JSON.stringify(this.player));
         this.isOn = true;
         this.round.loadQuestions();
     }
 
-    
+
 }
--- js/game.controller.js
+++ js/game.controller.js
@@ -1,6 +1,7 @@
 import { Question } from "./Question.js";
 import { Answer} from "./Answer.js";
 
+
 export const getRandomQuestion = (questions) => {
   const random = Math.floor(Math.random() * questions.length);
   return new Question(
@@ -16,4 +17,14 @@ const createAnswer = (answers) => {
     answerArray.push(new Answer(answer.answer, answer.correct));
   });
   return answerArray;
+}
+
+const answerValidation = (answer, answers) => {
+  let isCorrect = false;
+  answers.forEach(ans => {
+    if (ans.isCorrect) {
+      isCorrect = true;
+    }
+  });
+  return isCorrect;
 }
